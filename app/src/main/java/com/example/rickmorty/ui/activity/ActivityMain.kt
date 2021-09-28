00c4fd8300d6020ed0a137e04458b9e8f05c64d0
Vsevolod Menshov <mensseva24@gmail.com>
2021-09-28 16:14:42 +0300

    update_0

---
+++ app/src/main/java/com/example/rickmorty/ui/activity/ActivityMain.kt
@@ -0,0 +1,12 @@
+package com.example.rickmorty.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rickmorty.R
+
+class ActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
