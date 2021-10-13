ca49ce6b12f4a28f0cb54404ba37430f682e33db
whxli <xetherealme@gmail.com>
2021-10-13 17:39:07 +0300

    update_3

--- app/src/main/java/com/example/rickmorty/mvp/view/ActivityMainView.kt
+++ app/src/main/java/com/example/rickmorty/mvp/view/ActivityMainView.kt
@@ -1,7 +1,6 @@
 package com.example.rickmorty.mvp.view
 
 import moxy.MvpView
-import moxy.viewstate.strategy.alias.AddToEndSingle
 import moxy.viewstate.strategy.alias.OneExecution
 
 @OneExecution
